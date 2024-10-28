2f7c0afc1fcfc6d2a0c46bfe8643f3028b6dc00f
Luanyurtado <luanyurtado8@gmail.com>
2024-10-28 14:04:26 -0300

    Add files via upload

---
+++ entities/Medicamento.java
@@ -0,0 +1,69 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table
+public class Medicamento {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@NotNull
+	@Size(min = 10, message = "Minimo de 10 caracteres")
+	private String nome;
+	private String bula;
+	
+	private int idFornecedor;
+	private String dataValidade;
+
+	// métodos getters and setters
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getBula() {
+		return bula;
+	}
+
+	public void setBula(String bula) {
+		this.bula = bula;
+	}
+
+	public int getIdFornecedor() {
+		return idFornecedor;
+	}
+
+	public void setIdFornecedor(int idFornecedor) {
+		this.idFornecedor = idFornecedor;
+	}
+
+	public String getDataValidade() {
+		return dataValidade;
+	}
+
+	public void setDataValidade(String dataValidade) {
+		this.dataValidade = dataValidade;
+	}
+
+}
